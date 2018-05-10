980937a2b5443c3e05eb7d8bcc31e0ac6942d3f4
Jessie Ling <jealic@hotmail.com>
2018-05-10 13:41:00 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
 
   private
 
     def authenticate_admin
       unless current_user.admin?
-        flash[:alert] = "你不具管理權限唷！"
+        flash[:alert] = "You don't have our administrative license!"
         redirect_to root_path
     end
   end
