8a030ab72292ddb5744fe6aced90f247f0914ed5
Chiehyun <jealic@gmail.com>
2018-05-19 03:03:26 +0800

    Next & Previous buttons on show page(front desk)

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,14 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
   has_many :comments
+
+  def prev
+    Restaurant.where("id<?", id).last    
+  end
+
+  def next
+    Restaurant.where("id>?", id).first
+  end
+
+
 end
