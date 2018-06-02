2fbdbcc90376b116d14ace79268b950793d754ff
Chiehyun <jealic@gmail.com>
2018-06-02 18:28:24 +0800

    changed model _fix_like_datatype to PostgreSQL-specific casting for deploying to heroku

--- db/migrate/20180527093753_fix_like_datatype.rb
+++ db/migrate/20180527093753_fix_like_datatype.rb
@@ -1,6 +1,6 @@
 class FixLikeDatatype < ActiveRecord::Migration[5.1]
   def change
-    change_column :likes, :user_id, :integer
-    change_column :likes, :restaurant_id, :integer
+    change_column :likes, :user_id, 'integer USING user_id::integer)'
+    change_column :likes, :restaurant_id, 'integer USING restaurant_id::integer)'
   end
 end
