bce8e1205c9b00f5010b283cceeba78ca586bddc
Chiehyun <jealic@gmail.com>
2018-06-10 00:32:17 +0800

    add routes for friend_list

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,13 @@ Rails.application.routes.draw do
 
 
   resources :categories, only: :show
-  resources :users, only: [:show, :edit, :update, :index]
+  
+  resources :users, only: [:show, :edit, :update, :index] do
+    member do
+      get :friend_list
+    end
+  end
+  
   resources :followships, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
 
