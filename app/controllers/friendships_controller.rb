99cfc57c0a9f4668655d2ae98f3d4a01fa4970a0
Chiehyun <jealic@gmail.com>
2018-06-09 16:45:11 +0800

    add friend -1 btn on user index page and destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,12 +4,18 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
 
     if @friendship.save
-      flash[:notice] = "Friend +1"
+      flash[:notice] = "Friend +1 ☺"
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = "Whoops! I think you don't have friends..."
+      flash[:alert] = "Whoops! I don't think you have friends..."
       redirect_back(fallback_location: root_path)
     end
+  end
 
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id]).first
+    @friendship.destroy
+    flash[:alert] = "Friend -1 ☹"
+    redirect_back(fallback_location: root_path)
   end
 end
